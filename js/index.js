f477fd3fd4608b833426a4de5602e83f6d0e6107
GOUST201222 <goust201222@yandex.ru>
2023-03-19 22:58:40 +0300

    new

---
+++ js/index.js
@@ -0,0 +1,54 @@
+//Sidebar visible
+$('.sidebarTrigger[data-sidebar-id]').on('click', function(){
+  $('.navigation-compact_sidebar').addClass('visible');
+});
+//Hide on touch to close icon
+$('.sidebar .close').on('click touchstart', function(){
+  $('.navigation-compact_sidebar').removeClass('visible');
+});
+//Swiper
+var swiper = new Swiper(".mySwiper", {
+
+
+  spaceBetween: 16,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 1025px
+    640: {
+      slidesPerView: 6.5,
+      spaceBetween: 37
+    }
+  }
+  
+
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+//Swiper edit
+function swiperEdit(){
+  if ($(window).width() <= '768'){
+    $('#prevSlide').attr('src', 'assets/svg/arrow-prev-phone.svg');
+    $('#nextSlide').attr('src', 'assets/svg/arrow-next-phone.svg');
+  } else{
+    if ($(window).width() <= '1024'){
+      $('#prevSlide').attr('src', 'assets/svg/arrow-prev-tablet.svg');
+      $('#nextSlide').attr('src', 'assets/svg/arrow-next-tablet.svg');
+    }else{
+      $('#prevSlide').attr('src', 'assets/svg/arrow-prev.svg');
+      $('#nextSlide').attr('src', 'assets/svg/arrow-next.svg');
+  }
+  }
+};
+$(window).on('load resize',swiperEdit);
